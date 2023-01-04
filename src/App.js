8578f37f868b90dac7530e0cf298fa8992de8908
PomboObeso <matheus.gomes201@gmail.com>
2023-01-03 21:39:32 -0300

    organizando os components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem vindo ao react!
         </p>
         <a
           className="App-link"
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,7 @@
+function Banner() {
+    return (
+        <img src="/img/._banner.png" alt="Banner principal do Organo"/>
+    )
+}
+
+export default Banner;
