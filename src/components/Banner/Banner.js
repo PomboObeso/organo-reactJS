e6bc4596f4fd1719fb965321b4d41c3c0c632173
PomboObeso <matheus.gomes201@gmail.com>
2023-01-03 21:43:32 -0300

    Bannerjs  - inicio

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,6 @@
 function Banner() {
     return (
-        <img src="/img/._banner.png" alt="Banner principal do Organo"/>
+        <img src="/img/banner.png" alt="Banner principal do Organo"/>
     )
 }
 
