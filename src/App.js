dadd745b6aa25d062645491edcbed3b8d33793e8
PomboObeso <matheus.gomes201@gmail.com>
2023-01-05 20:55:09 -0300

    form- inputs feito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Banner from './components/Banner/index.js';
-import CampoTexto from './components/CampoTexto/index.js';
+import Formulario from './components/Formulario/index.js';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto/>
+      <Formulario/>
     </div>
   );
 }
--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,10 +1,10 @@
 import './CampoTexto.css'
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
     return (
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder='Digite seu nome'/>
+            <label>{props.label}</label>
+            <input placeholder={props.placeholder}/>
         </div>
     )
 }
