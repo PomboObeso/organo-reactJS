ef57f76e9a6cd70a586501a49fe3b9516d2af6a5
PomboObeso <matheus.gomes201@gmail.com>
2023-01-04 22:28:37 -0300

    campo texto : nome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Banner from './components/Banner/Banner';
+import Banner from './components/Banner/index.js';
+import CampoTexto from './components/CampoTexto/index.js';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      
+      <CampoTexto/>
     </div>
   );
 }
---
+++ src/components/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder='Digite seu nome'/>
+        </div>
+    )
+}
+
+export default CampoTexto;
