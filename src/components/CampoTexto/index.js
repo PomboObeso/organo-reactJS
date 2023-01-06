a76790c02f2e09da312f8931b42d9a1d0886bdc2
PomboObeso <matheus.gomes201@gmail.com>
2023-01-06 17:28:51 -0300

    implementacao do selection

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -4,7 +4,7 @@ const CampoTexto = (props) => {
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input placeholder={props.placeholder} required/>
         </div>
     )
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,7 +1,18 @@
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'UX e Design', 
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className='formulario'>
             <form>
@@ -9,6 +20,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label="Times" itens={times}/>
             </form>
         </section>
     )
