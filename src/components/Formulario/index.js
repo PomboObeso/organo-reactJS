29c363a65d1c58f598e28e481c061f48d94f40aa
PomboObeso <matheus.gomes201@gmail.com>
2023-01-06 21:29:44 -0300

    botao component - criar card

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
@@ -21,6 +22,9 @@ const Formulario = () => {
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
                 <ListaSuspensa label="Times" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
