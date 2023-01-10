eea3ee1fe696f2d6b62a204071962006cb4271a7
PomboObeso <matheus.gomes201@gmail.com>
2023-01-10 11:06:17 -0300

    criacao de card do colaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/PomboObeso.png' alt='Imagem de Perfil - Colaborador' />
+            </div>
+            <div className='rodape'>
+                <h4>Matheus</h4>
+                <h5>Aluno</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
@@ -7,6 +8,7 @@ const Time = (props) => {
     return (
         <section className='time' style={css}>
             <h3 style={{borderColor:props.corPrimaria}} >{props.nome}</h3>
+            <Colaborador/>
         </section>
     )
 }
